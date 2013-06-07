07df1e95a4a21bd8d5f1369d3706c849bbe74a51
Luca Postregna <luca.postregna@gmail.com>
2013-06-07 16:07:19 +0200

    fix

--- templates/en_template.py
+++ templates/en_template.py
@@ -4,7 +4,7 @@ SRC = "/home/lucapost/repo/nicelyventilated.it/src/en"
 DST = "./en"
 LANG = "en"
 TITLE = "home"
-DESC = "Apartment in the center of Rome, in the Esquilino neighborhood near the Termini station"
-SUBTITLE = "apartment in the center of Rome, in the <span>Esquilino neighborhood</span> near the <span>Termini station</span>"
+DESC = "Apartment for rent in the center of Rome, in the Esquilino neighborhood near the Termini station"
+SUBTITLE = "apartment for rent in the center of Rome, in the <span>Esquilino neighborhood</span> near the <span>Termini station</span>"
 
 execfile('templates/default_template.py')
--- templates/it_template.py
+++ templates/it_template.py
@@ -2,7 +2,7 @@ import datetime
 
 LANG = "it"
 TITLE = "casa"
-SUBTITLE = "appartamento al centro di Roma nel <span>quartiere Esquilino</span> vicino alla <span>stazione Termini</span>"
-DESC = "Appartamento al centro di Roma nel quartiere Esquilino vicino alla stazione Termini"
+SUBTITLE = "appartamento in affitto al centro di Roma nel <span>quartiere Esquilino</span> vicino alla <span>stazione Termini</span>"
+DESC = "Appartamento in affitto al centro di Roma nel quartiere Esquilino vicino alla stazione Termini"
 
 execfile('templates/default_template.py')
