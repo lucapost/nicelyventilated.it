f394f9e336425039d3ceb1a0f4bf2658285fe8a8
Luca Postregna <luca.postregna@gmail.com>
2013-06-07 18:51:26 +0200

    fix

--- templates/en_template.py
+++ templates/en_template.py
@@ -5,6 +5,6 @@ DST = "./en"
 LANG = "en"
 TITLE = "home"
 DESC = "Apartment for rent in the center of Rome, in the Esquilino neighborhood near the Termini station"
-SUBTITLE = "apartment for rent in the center of Rome, in the <span>Esquilino neighborhood</span> near the <span>Termini station</span>"
+SUBTITLE = "Apartment for rent in the center of Rome, in the <span>Esquilino neighborhood</span> near the <span>Termini station</span>"
 
 execfile('templates/default_template.py')
