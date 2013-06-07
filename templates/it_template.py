46bbd3bc09adb324b7631b3bbbab5d10af45a103
Luca Postregna <luca.postregna@gmail.com>
2013-06-07 14:25:08 +0200

    fix

--- templates/it_template.py
+++ templates/it_template.py
@@ -7,7 +7,7 @@ ENG = ""
 DEU = ""
 PREFIX = "/"
 TITLE = "casa"
-DESC = "appartamento al centro di Roma nel quartiere Esquilino vicino alla stazione Termini"
+DESC = "Appartamento al centro di Roma nel quartiere Esquilino vicino alla stazione Termini"
 SUBTITLE = "appartamento al centro di Roma nel <span>quartiere Esquilino</span> vicino alla <span>stazione Termini</span>"
 
 execfile('templates/default_template.py')
