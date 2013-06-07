99001dd33867c81b089dd7ebd5bb114248a15214
Luca Postregna <luca.postregna@gmail.com>
2013-06-07 15:34:49 +0200

    fix

--- templates/default_template.py
+++ templates/default_template.py
@@ -36,7 +36,6 @@ def header(node):
 	<!--[if IE 8]>    <html class="no-js lt-ie9" lang="''' + LANG + '''"> <![endif]-->
 	<!--[if gt IE 8]><!--> <html class="no-js" lang="''' + LANG + '''"> <!--<![endif]-->
 	<head>
-        <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
        	<meta charset="utf-8" />
        	<meta name="author" content="lucapost" />
 	    <meta name="description" content="''' + DESC + '''" />
@@ -66,6 +65,14 @@ def header(node):
 				});
 			});
 		</script> 
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-6164762-14', 'nicelyventilated.it');
+            ga('send', 'pageview');
+        </script>
 	</head>
 	<body>
 		<header>
